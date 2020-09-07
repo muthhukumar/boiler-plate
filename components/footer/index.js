f0ca90f342735c8dd46569dbdee66edd32eba648
0-void <muthukumarperiyasamy.mk@gmail.com>
2020-09-07 22:15:49 +0530

    try 2

--- components/footer/index.js
+++ components/footer/index.js
@@ -7,9 +7,22 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 
 const Footer = () => {
     const [queryText, setQueryText] = useState('')
+
+    const handleSubmit = e => {
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({ 'form-name': 'plate request', queryText }),
+        })
+            .then(() => {})
+            .catch(error => alert(error))
+
+        e.preventDefault()
+    }
+
     return (
         <div className="border border-grey-500 flex p-4 bg-white shadow-md items-center justify-around text-black my-2 rounded-lg">
-            <div>
+            <div className="mx-4">
                 <Link href="https://www.twitter.com/_0_void_">
                     <a className="hover:cursor-pointer" target="_blank">
                         <TwitterIcon className="mx-2" fontSize="large" />
@@ -26,18 +39,9 @@ const Footer = () => {
                     </a>
                 </Link>
             </div>
-            <form
-                name="plate-request"
-                netlify
-                netlify-honeypot="bot-field"
-                hidden
-            >
-                <input type="text" name="plate" />
-            </form>
-            <form method="post">
-                <input type="hidden" name="form-name" value="plate-request" />
+            <form onSubmit={handleSubmit}>
                 <input
-                    className="text-lg border border-grey-400 rounded px-3 py-2 flex-1 focus:outline-none focus:outline-grey-500 mx-2"
+                    className="text-lg border border-grey-400 rounded px-3 py-2 flex-1 focus:outline-none focus:outline-grey-500"
                     placeholder="request plate"
                     value={queryText}
                     name="plate"
@@ -46,7 +50,7 @@ const Footer = () => {
                 />
                 <button
                     type="submit"
-                    className="text-lg bg-gradient-to-r from-teal-400 to-blue-500 focus:from-pink-500 focus:to-orange-500 text-white font-semibold px-4 py-2 rounded"
+                    className="m-2 text-lg bg-gradient-to-r from-teal-400 to-blue-500 focus:from-pink-500 focus:to-orange-500 text-white font-semibold px-4 py-2 rounded"
                 >
                     submit
                 </button>
